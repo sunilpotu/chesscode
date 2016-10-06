a7d9d5aa025c084d21534c32add667d80199e04f
sunilreddypotu <sunil.potu@xcubelabs.com>
2016-10-06 11:49:12 +0530

    add classes for chess pieces

---
+++ chess1.java
@@ -0,0 +1,65 @@
+package sunil;
+
+
+class  king{
+	void kingfunctionality(){
+		
+	}
+	
+}
+
+class  Queen {
+	void queenfunctionality(){
+		
+	}
+	
+}
+
+class Rook {
+	void rookfunctionality(){
+		
+	}
+	
+	
+}
+class Bishop {
+	void bishopfunctionalty(){
+		
+	}
+	
+}
+class Knight {
+	void Knightfunctionality(){
+		
+	}
+	
+} 
+
+class pawm {
+	void pawnfunctionality(){
+		
+	}
+	
+}
+public class Chess {
+	public static void main(String args[]){  
+		king k=new king();  
+		Queen q=new Queen();  
+		Rook r=new Rook();
+		Bishop b =new Bishop();
+		Knight kn =new Knight();
+		pawm pw = new pawm();
+		
+		k.kingfunctionality();
+		q.queenfunctionality();
+		r.rookfunctionality();
+		b.bishopfunctionalty();
+		kn.Knightfunctionality();
+		pw.pawnfunctionality();
+		
+		  
+		}  
+
+    
+}  
+
